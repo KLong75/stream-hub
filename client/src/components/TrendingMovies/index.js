d4361ca7a44fb62a85abe69594e006b71610bd1b
Kevin Long <98487770+KLong75@users.noreply.github.com>
2023-09-14 22:43:04 -0500

    styles for trending movies

--- client/src/components/TrendingMovies/index.js
+++ client/src/components/TrendingMovies/index.js
@@ -5,6 +5,7 @@ import { fetchTrendingMoviesPageOne } from "../../utils/apiCalls";
 // import from material-ui
 import Button from "@mui/material/Button";
 import { Dialog, DialogTitle, DialogContent } from "@mui/material";
+
 // import from utils
 import { CACHE_DURATION_ONE_WEEK, formatDate } from "../../utils/utils";
 import { useTitleSelectionTMDBId } from "../../utils/useTitleSelectionTMDBId";
@@ -121,7 +122,7 @@ const TrendingMovies = () => {
 
   return (
     <>
-      <h3 style={{ marginBottom: "0" }}>Trending Movies</h3>
+      <h3 className={styles.category}>Trending Movies</h3>
       <Swiper
         style={{
           "--swiper-navigation-color": "#000000",
@@ -149,34 +150,55 @@ const TrendingMovies = () => {
           <SwiperSlide
             className={styles.slide}
             key={movie.id}
-            // style={{
-            //   backgroundImage: `url(https://image.tmdb.org/t/p/w200/${movie.poster_path})`,
-            // }}
+            style={{
+              backgroundImage: `url(https://image.tmdb.org/t/p/w200/${movie.poster_path}), linear-gradient(315deg, #43cea2 0%,  #185a9d 85%)`,
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              // backgroundSize: "cover",
+              backgroundColor: ""
+            }}
+            onClick={() => {
+              const customEvent = {
+                preventDefault: () => {},
+                target: { value: `movie-${movie.id}` },
+              };
+              handleTitleSelected(customEvent);
+            }}
           >
-            <p>
-              <strong>{movie.title}</strong>
-            </p>
-            <p>
+            <h4 className={styles.movieTitle}>
+              <strong className={styles.movieTitle}>{movie.title}</strong>
+            </h4>
+
+            {/* <img
+              src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+              alt={movie.title}
+              className={styles.img}
+            /> */}
+            <h5 className={styles.movieGenres}>
               <strong>
                 {movie.genre
                   .map((id) => genreList[id])
                   .filter(Boolean)
-                  .slice(0,2)
+                  .slice(0, 2)
                   .join(", ")}
               </strong>
-            </p>
-            <img
-              src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
-              alt={movie.title}
-              className={styles.img}
-            />
-            <p>
+            </h5>
+            <p className={styles.releaseDate}>
               <strong>Released on {movie.release_date}</strong>
             </p>
-            <Button 
+
+            {/* <Button
               variant="contained"
-              onClick={() => handleOverviewClick(movie.overview, movie.title, movie.id, movie.poster_path)}>
-              Overview
+              onClick={() =>
+                handleOverviewClick(
+                  movie.overview,
+                  movie.title,
+                  movie.id,
+                  movie.poster_path
+                )
+              }
+            >
+              Plot
             </Button>
             <Button
               variant="contained"
@@ -184,28 +206,31 @@ const TrendingMovies = () => {
               onClick={handleTitleSelected}
             >
               More Details
-            </Button>
+            </Button> */}
           </SwiperSlide>
         ))}
       </Swiper>
+
       {/* Modal for showing overview */}
       <Dialog open={isModalOpen} onClose={handleCloseModal}>
-        <DialogTitle className={styles.overviewTitle}>{currentTitle}</DialogTitle>
+        <DialogTitle className={styles.overviewTitle}>
+          {currentTitle}
+        </DialogTitle>
         <DialogContent>
-        <img
-              src={`https://image.tmdb.org/t/p/w200/${currentTitlePoster}`}
-              alt={currentTitle}
-              className={styles.img}
-            />
+          <img
+            src={`https://image.tmdb.org/t/p/w200/${currentTitlePoster}`}
+            alt={currentTitle}
+            className={styles.img}
+          />
           <p className={styles.overviewText}>{currentOverview}</p>
         </DialogContent>
         <Button
-              variant="contained"
-              value={`movie-${currentTitleId}`}
-              onClick={handleTitleSelected}
-            >
-              More Details
-            </Button>
+          variant="contained"
+          value={`movie-${currentTitleId}`}
+          onClick={handleTitleSelected}
+        >
+          More Details
+        </Button>
       </Dialog>
     </>
   );
