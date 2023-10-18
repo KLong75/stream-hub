4a21acb0838875a961999d818d15048cbd8f3d0a
Kevin Long <98487770+KLong75@users.noreply.github.com>
2023-10-18 16:43:40 -0500

    troubleshooting deploment issues

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  context: authMiddleware,
+  persistedQueries: {
+    cache: "bounded",
+  },
 });
 
 const app = express();
