eea7ed7f493b976efaef26753c62ff4c6dd5da06
Kevin Long <98487770+KLong75@users.noreply.github.com>
2023-10-29 23:29:52 -0500

    fix size of source logos

--- client/src/components/SourceLink/index.js
+++ client/src/components/SourceLink/index.js
@@ -1,16 +1,24 @@
 // import from mui
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 
-
 const SourceLink = ({ url, logoSrc, altText }) => {
   if (!url) {
     return null;
   }
 
   return (
-    <Grid xs={12}>
+    <Grid xs={12} sx={{ height: "3.5rem", overflow: "hidden" }}>
       <a href={url} target="_blank" rel="noopener noreferrer">
-        <img style={{ height: "4rem" }} src={logoSrc} alt={altText} />
+        <img
+          style={{
+            maxWidth: "6rem",
+            maxHeight: "auto",
+            display: "block",
+            margin: "0 auto",
+          }}
+          src={logoSrc}
+          alt={altText}
+        />
       </a>
     </Grid>
   );
