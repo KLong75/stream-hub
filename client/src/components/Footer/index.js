d4cfccfcd575cb011efe4cd7f7f9cf7c69b4ad30
Kevin Long <98487770+KLong75@users.noreply.github.com>
2023-11-06 15:01:00 -0600

    add on hover to footer link

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,32 +1,40 @@
 // import images
 import filmStrip from "../../assets/images/FilmStrip_PNG.png";
 // import from mui
-import Grid from '@mui/material/Unstable_Grid2';
+import Grid from "@mui/material/Unstable_Grid2";
 import { Link } from "@mui/material";
 // import from react-router-dom
-import {useLocation} from 'react-router-dom';
-
+import { useLocation } from "react-router-dom";
+// import styles
+import styles from "./Footer.module.css";
 
 const Footer = () => {
-
   const location = useLocation();
-  if (location.pathname === '/' ) return null;
+  if (location.pathname === "/") return null;
 
   return (
     <footer
-      style={{ 
-        position: "relative", 
+      style={{
+        position: "relative",
         bottom: 0,
-        height: "5rem", 
+        height: "5rem",
         backgroundImage: `url(${filmStrip})`,
-        backgroundRepeat: 'repeat-x',
-        backgroundSize: 'auto 5rem',
-        backgroundPosition: 'left bottom',
-      }}
-    >
+        backgroundRepeat: "repeat-x",
+        backgroundSize: "auto 5rem",
+        backgroundPosition: "left bottom",
+      }}>
       <Grid container spacing={2} justifyContent="center" alignItems="center">
-        <Grid style={{ position: "relative", marginTop: '-1.75rem' }}>
-          <span >©2023 <Link underline= 'none' target='_blank' rel='noreferrer' href="https://kevinlong.dev/">www.kevinlong.dev</Link></span>  
+        <Grid style={{ position: "relative", marginTop: "-1.75rem" }}>
+          <span>
+            <Link
+              className={styles.footerLink}
+              underline="none"
+              target="_blank"
+              rel="noreferrer"
+              href="https://kevinlong.dev/">
+              ©2023 www.kevinlong.dev
+            </Link>
+          </span>
         </Grid>
       </Grid>
     </footer>
